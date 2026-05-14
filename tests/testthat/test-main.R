c6a2ca702f1c8a366f841aaa75af8f9c9cc0754e
benetsugarboy <benet1one@gmail.com>
2026-05-14 10:26:10 +0200

    Improved tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,22 +1,52 @@
 
+test_that("main", {
+    one <- 1
 
-f <- function(x, a, b = 2) {
-    asdf <- (rowSums(x) - a)
-    sum(asdf) - (b - 3)^2
-}
+    f <- function(x, a, b = 2) {
+        y <- (rowSums(x) - a)^4
+        sum(y) + (a[1] - 25)^2 + one/b
+    }
 
-problem <- objective(f) |>
-    variable(x, dimensions = c(2, 3), upper = 10) |>
-    variable(a, dimensions = 2, lower = 4:3) |>
-    maximize()
+    s <- objective(f) |>
+        variable(x, dimensions = c(2, 3), upper = 10) |>
+        variable(a, dimensions = 2, lower = c(0, 40)) |>
+        minimize(max_iter = 10e3)
 
-print(problem)
+    print(s)
+
+    expect_equal(
+        do.call(f, s$solution)[1],
+        s$objective_value
+    )
+    expect_equal(
+        rowSums(s$solution$x) |> round(),
+        c(25, 30)
+    )
+    expect_equal(
+        s$solution$a |> round() |> c(),
+        c(25, 40)
+    )
 
 
-problem <- objective(f) |>
-    variable(x, dimensions = c(2, 3), upper = 10) |>
-    variable(a, dimensions = 2, lower = 4:3) |>
-    variable(b, dimensions = 1, lower = 1, upper = 8) |>
-    maximize()
+    sb <- objective(f) |>
+        variable(x, dimensions = c(2, 3), upper = 10) |>
+        variable(a, dimensions = 2, lower = c(0, 40)) |>
+        variable(b, lower = 0, upper = 4) |>
+        minimize(max_iter = 10e3)
 
-print(problem)
+    print(sb)
+
+    expect_equal(
+        do.call(f, sb$solution)[1],
+        sb$objective_value
+    )
+    expect_equal(
+        sb$solution$b[1],
+        4
+    )
+
+    expect_error(
+        objective(f) |> variable(d),
+        "`d` is not a parameter in the objective function"
+    )
+})
